8e524c5d768326debf9b92203788c45ab25362e4
JeffreyChen <jeffreychen@JeffreyChens-MacBook-Pro.local>
2020-02-18 19:16:56 -0600

    test(stack): update bucket stack and add test

--- lib/S3BucketStack/index.ts
+++ lib/S3BucketStack/index.ts
@@ -1,24 +1,14 @@
 import * as cdk from '@aws-cdk/core';
 import { Bucket } from '@aws-cdk/aws-s3';
-import { PolicyStatement, Effect, AccountPrincipal, } from '@aws-cdk/aws-iam';
 
 export default class S3BucketStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
     const bucketName = 'wedding-site-hosting-bucket'
 
-    const bucket = new Bucket(this, 'WeddingSiteHostingBucket', {
+    new Bucket(this, 'WeddingSiteHostingBucket', {
         bucketName:bucketName,
+        publicReadAccess: true
     })
-
-    const statement = new PolicyStatement({
-        effect: Effect.ALLOW,
-        principals:[new AccountPrincipal('*')],
-        actions:['s3:GetObject'],
-        resources: ['arn:aws:s3:::wedding-site-hosting-bucket/*']
-    })
-
-    bucket.addToResourcePolicy(statement);
   }
 }
